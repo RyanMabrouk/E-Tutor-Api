61247dc0dccadf424f59a8cf7f1f5d53eb472113
Rayen Mabrouk <mabroukr911@gmail.com>
2024-04-15 10:02:06 +0100

    fix(file storage): fixed files controller

--- src/routes/files/infrastructure/uploader/local/files.controller.ts
+++ src/routes/files/infrastructure/uploader/local/files.controller.ts
@@ -1,21 +1,22 @@
+import * as fs from 'fs';
+import * as path from 'path';
 import {
   BadRequestException,
   Controller,
   Get,
   Param,
   Post,
+  Req,
   Response,
-  UploadedFile,
   UseGuards,
-  UseInterceptors,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FilesLocalService } from './files.service';
-import { FileInterceptor, MemoryStorageFile } from '@blazity/nest-file-fastify';
-import { FastifyReply } from 'fastify';
+import { FastifyReply, FastifyRequest } from 'fastify';
 import { join } from 'path';
 import { appRoot } from 'src/main';
 import { createReadStream } from 'fs';
+import { randomStringGenerator } from '@nestjs/common/utils/random-string-generator.util';
 @Controller({
   path: 'files',
   version: '1',
@@ -25,10 +26,29 @@ export class FilesLocalController {
 
   @UseGuards(AuthGuard('jwt'))
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
-  async uploadFile(@UploadedFile() file: MemoryStorageFile) {
-    console.log('🚀 ~ FilesLocalController ~ uploadFile ~ file:', { ...file });
-    return this.filesService.create(file);
+  async uploadFile(@Req() request: FastifyRequest & any) {
+    if (!request.isMultipart()) {
+      throw new BadRequestException('No files in request');
+    }
+    const data = await request.file();
+    if (!data) {
+      throw new BadRequestException('File upload failed');
+    }
+    if (!data.filename.match(/\.(jpg|jpeg|png|gif)$/i)) {
+      throw new BadRequestException('Unsupported file type');
+    }
+    const filename = `${randomStringGenerator()}.${data.filename.split('.').pop()?.toLowerCase()}`;
+    data.filename = filename;
+
+    const filepath = path.join('./files', filename);
+
+    await new Promise<void>((resolve, reject) => {
+      const writeStream = fs.createWriteStream(filepath);
+      data.file.pipe(writeStream);
+      data.file.on('end', () => resolve());
+      data.file.on('error', (error) => reject(error));
+    });
+    return this.filesService.create(filename);
   }
 
   @Get(':path')
