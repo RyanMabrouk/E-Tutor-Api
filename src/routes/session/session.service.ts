25b7237be1237386d8b9c6d96571171a08269980
Rayen Mabrouk <mabroukr911@gmail.com>
2024-04-25 15:09:37 +0100

    refactor: refactor session service to use GeneralDomainKeysWithId in create and update methods

--- src/routes/session/session.service.ts
+++ src/routes/session/session.service.ts
@@ -4,6 +4,7 @@ import { EntityCondition } from 'src/utils/types/entity-condition.type';
 import { SessionRepository } from './infrastructure/persistence/session.repository';
 import { Session } from './domain/session';
 import { NullableType } from 'src/utils/types/nullable.type';
+import { GeneralDomainKeysWithId } from 'src/shared/domain/general.domain';
 
 @Injectable()
 export class SessionService {
@@ -13,17 +14,13 @@ export class SessionService {
     return this.sessionRepository.findOne(options);
   }
 
-  create(
-    data: Omit<Session, 'id' | 'createdAt' | 'updatedAt' | 'deletedAt'>,
-  ): Promise<Session> {
+  create(data: Omit<Session, GeneralDomainKeysWithId>): Promise<Session> {
     return this.sessionRepository.create(data);
   }
 
   update(
     id: Session['id'],
-    payload: Partial<
-      Omit<Session, 'id' | 'createdAt' | 'updatedAt' | 'deletedAt'>
-    >,
+    payload: Partial<Omit<Session, GeneralDomainKeysWithId>>,
   ): Promise<Session | null> {
     return this.sessionRepository.update(id, payload);
   }
