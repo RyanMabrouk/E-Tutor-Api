d1584a8b89fefd3c69aa047fd75ff70af7852fc9
Rayen Mabrouk <mabroukr911@gmail.com>
2024-04-26 15:49:11 +0100

    fix: setup db scripts

--- startup.relational.ci.sh
+++ startup.relational.ci.sh
@@ -3,8 +3,8 @@ set -e
 
 /opt/wait-for-it.sh postgres:5432
 npm run migration:run
-npm run seed:run:relational
-npm run start:prod > prod.log 2>&1 &
+npm run seed
+npm run prod > prod.log 2>&1 &
 /opt/wait-for-it.sh maildev:1080
 /opt/wait-for-it.sh localhost:3000
 npm run lint
--- startup.relational.dev.sh
+++ startup.relational.dev.sh
@@ -3,5 +3,5 @@ set -e
 
 /opt/wait-for-it.sh postgres:5432
 npm run migration:run
-npm run seed:run:relational
-npm run start:prod
+npm run seed
+npm run prod
