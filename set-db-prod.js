513cd9141d518f18aaf5f9c7bbe984067e718905
Rayen Mabrouk <mabroukr911@gmail.com>
2024-05-02 12:20:37 +0100

    refactor: added scripts to automate the migration proccess in docker

---
+++ set-db-prod.js
@@ -0,0 +1,12 @@
+// set-db-host.js
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
+const envPath = path.join(__dirname, '.env');
+let envFile = fs.readFileSync(envPath, 'utf8');
+
+envFile = envFile.replace(/(DATABASE_HOST=).*/, `$1postgres`);
+
+fs.writeFileSync(envPath, envFile);
